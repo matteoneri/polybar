45035d3c7b0e914e8029a6b245b4ceb32aea564a
Matteo Neri <nremtt@gmail.com>
2021-02-25 08:59:08 +0100

    updated gcal

--- scripts/next_gcal_event.sh
+++ scripts/next_gcal_event.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/gcalcli --calendar "Matteo Neri @FNA" --calendar "nremtt@gmail.com" --calendar "matteo.neri920114@gmail.com" agenda "12am" "11pm" --nostarted --tsv | head -1 | awk '{print " " $2"-"$4, $5, $6, $7, $8}'
+/home/matteo/miniforge3/bin/gcalcli --calendar "Matteo Neri @FNA" --calendar "nremtt@gmail.com" --calendar "matteo.neri920114@gmail.com" agenda "12am" "11pm" --nostarted --tsv | head -1 | awk '{print " " $2"-"$4, $5, $6, $7, $8}'
--- scripts/next_gcal_events_of_the_day.sh
+++ scripts/next_gcal_events_of_the_day.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo Next events in gcal:
-/usr/bin/gcalcli --calendar "Matteo Neri @FNA" --calendar "nremtt@gmail.com" --calendar "matteo.neri920114@gmail.com" agenda "12am" "11pm" --nostarted --tsv |
+## GCACLI=/usr/bin/gcalclii
+GCACLI=(/home/matteo/miniforge3/bin/gcalcli)
+
+
+/home/matteo/miniforge3/bin/gcalcli --calendar "Matteo Neri @FNA" --calendar "nremtt@gmail.com" --calendar "matteo.neri920115@gmail.com" agenda "12am" "11pm" --nostarted --tsv |
     awk '{print " " $2"-"$4, $5, $6, $7, $8}' |
     while read -r line; do
         sleep 0.02
@@ -8,6 +12,6 @@ echo Next events in gcal:
         sleep 2
     done
 
-/usr/bin/gcalcli --calendar "Matteo Neri @FNA" --calendar "nremtt@gmail.com" --calendar "matteo.neri920114@gmail.com" agenda "12am" "11pm" --nostarted --tsv |
+/home/matteo/miniforge3/bin/gcalcli --calendar "Matteo Neri @FNA" --calendar "nremtt@gmail.com" --calendar "matteo.neri920114@gmail.com" agenda "12am" "11pm" --nostarted --tsv |
     head -1 |
     awk '{print " " $2"-"$4, $5, $6, $7, $8}'
